af647623b55c3dcfb733f4ba3cc1b83ced57b286
gfftech2025 <gfftech@iamai.in>
2025-06-13 12:26:07 +0530

    Modified based on the call
    Update package dependencies, modify partner schema, and enhance documentation
    
    - Updated React, React DOM, and React Router DOM to latest versions.
    - Added new dependencies for drag-and-drop functionality and import/export features.
    - Removed unused fields from the partner schema and updated related API documentation to reflect changes in endpoints and response structures.
    - Adjusted content types and their definitions to improve clarity and maintainability.

---
+++ src/api/partners-home-page/routes/partners-home-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * partners-home-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::partners-home-page.partners-home-page');
