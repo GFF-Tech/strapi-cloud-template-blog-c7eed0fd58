30b7677cd647d035f72a015f3dade8699e0e8598
gfftech2025 <gfftech@iamai.in>
2025-06-21 16:47:40 +0530

    Update mobile app FAQ schema and documentation
    - Added new mobile app FAQ attributes in contentTypes.d.ts.
    - Updated full_documentation.json to reflect changes in mobile app FAQ endpoints and response schemas.
    - Adjusted generation date in full_documentation.json.

---
+++ src/api/mobile-app-faq/routes/mobile-app-faq.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-app-faq router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mobile-app-faq.mobile-app-faq');
---
+++ src/api/mobile-app-faq/services/mobile-app-faq.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-app-faq service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mobile-app-faq.mobile-app-faq');
