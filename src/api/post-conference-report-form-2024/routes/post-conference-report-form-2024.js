75b04c9c08f0ac90907352fffd85e10262e5958c
akshay-langoor <gfftech@iamai.in>
2025-06-25 13:03:04 +0530

    Added API token auth to other 2 more form content types
    -Applied global api-token-auth policy to Plan Your stay form and Post Conference Report Form 2024 routes

--- src/api/post-conference-report-form-2024/routes/post-conference-report-form-2024.js
+++ src/api/post-conference-report-form-2024/routes/post-conference-report-form-2024.js
@@ -4,6 +4,23 @@
  * post-conference-report-form-2024 router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::post-conference-report-form-2024.post-conference-report-form-2024');
+module.exports = {
+    routes: [
+       {
+      method: 'GET',
+      path: '/post-conference-report-forms-2024-auth',
+      handler: 'post-conference-report-form-2024.find',
+      config: {
+        policies: ['global::api-token-auth'],
+      },
+    },
+      {
+        method: 'POST',
+        path: '/post-conference-report-forms-2024',
+        handler: 'post-conference-report-form-2024.create',
+        config: {
+          policies: [],
+        },
+      },
+    ],
+  };
