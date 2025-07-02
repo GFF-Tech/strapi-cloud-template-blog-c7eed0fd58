404387921d683d8b7a6f18cc21ba4fdd8e7f31de
gfftech2025 <gfftech@iamai.in>
2025-07-03 02:01:40 +0530

    Update API documentation and type definitions for Regular Pages
    - Changed endpoint references from "/salutations" to "/regular-pages" and updated related operation IDs and response schemas.
    - Updated the generation date in the full documentation.
    - Added new component schemas for CommonComponentsHeroSection and CommonComponentsRegText.
    - Introduced RegularPage content type with dynamic zones for page sections.

---
+++ src/api/regular-page/services/regular-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * regular-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::regular-page.regular-page');
