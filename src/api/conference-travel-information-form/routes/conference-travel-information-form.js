00af0b00b918fdd58397517792c872a228e0adaa
gfftech2025 <gfftech@iamai.in>
2025-07-08 16:11:23 +0530

    Update API documentation and add Conference Travel Information Form schema
    - Updated the x-generation-date in full_documentation.json.
    - Renamed endpoints related to confirmed speakers to conference travel information forms.
    - Updated response schemas and operation IDs accordingly.
    - Added new Conference Travel Information Form schema in contentTypes.d.ts.

---
+++ src/api/conference-travel-information-form/routes/conference-travel-information-form.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * conference-travel-information-form router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::conference-travel-information-form.conference-travel-information-form');
