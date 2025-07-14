4ec445ebcf85553b6db0bba41468df5c3a76901e
akshay-langoor <gfftech@iamai.in>
2025-07-14 09:05:00 +0530

    Added 2 New Content Types for Mobile App

---
+++ src/api/mobile-app-networking-goal/controllers/mobile-app-networking-goal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-app-networking-goal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mobile-app-networking-goal.mobile-app-networking-goal');
---
+++ src/api/mobile-app-purposeof-attendance/services/mobile-app-purposeof-attendance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mobile-app-purposeof-attendance service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mobile-app-purposeof-attendance.mobile-app-purposeof-attendance');
